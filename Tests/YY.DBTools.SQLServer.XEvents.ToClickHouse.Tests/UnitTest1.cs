5f966c885bfdee1aff405cad97a06cb3439f5e21
YPermitin <ypermitin@yandex.ru>
2021-01-07 00:18:06 +0500

    Подготовил каркас проекта для библиотек к SQL Server

---
+++ Tests/YY.DBTools.SQLServer.XEvents.ToClickHouse.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace YY.DBTools.SQLServer.XEvents.ToClickHouse.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
