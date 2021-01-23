148baa71dc7009f12e9de0256fff75a45e9d59e2
YPermitin <ypermitin@yandex.ru>
2021-01-24 00:27:02 +0500

    Добавил скрипты публикации Nuget-библиотек

---
+++ Scripts/deploy.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+API_KEY = $1
+
+dotnet nuget push ./YY.DBTools.Core/bin/Release/YY.DBTools.Core.*.nupkg -k $1 -s https://api.nuget.org/v3/index.json --skip-duplicate
+dotnet nuget push ./YY.DBTools.SQLServer.XEvents/bin/Release/YY.DBTools.SQLServer.XEvents.*.nupkg -k $1 -s https://api.nuget.org/v3/index.json --skip-duplicate
+dotnet nuget push ./YY.DBTools.SQLServer.XEvents.ToClickHouse/bin/Release/YY.DBTools.SQLServer.XEvents.ToClickHouse.*.nupkg -k $1 -s https://api.nuget.org/v3/index.json --skip-duplicate
